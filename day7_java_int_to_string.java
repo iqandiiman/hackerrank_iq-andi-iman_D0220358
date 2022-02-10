8b2cf8c7cf0f93dffa596ea157733424b87738d5
iqandiiman <iqandiiman1910@gmail.com>
2022-02-10 09:41:53 +0700

    day6, day7, day8

---
+++ day7_java_int_to_string.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.security.*;
+public class day7_java_int_to_string {
+ public static void main(String[] args) {
+
+  DoNotTerminate.forbidExit();
+
+  try {
+   Scanner in = new Scanner(System.in);
+   int n = in .nextInt();
+   in.close();
+   //String s=???; Complete this line below
+
+   //Write your code here
+   //String s = ""+n; ->cara pertama
+   //String s = String.valueOf(n); ->cara kedua
+   //String s = Integer.toString(n); ->cara ketiga
+   //String s = String.format("%d", n); ->cara keempat
+   
+   //cara kelima :
+   Integer i = new Integer(n);
+    StringBuilder sb = new StringBuilder();
+    sb.append(i);
+    String s = sb.toString();
+
+   
+   if (n == Integer.parseInt(s)) {
+    System.out.println("Good job");
+   } else {
+    System.out.println("Wrong answer.");
+   }
+  } catch (DoNotTerminate.ExitTrappedException e) {
+   System.out.println("Unsuccessful Termination!!");
+  }
+ }
+}
+
+//The following class will prevent you from terminating the code using exit(0)!
+class DoNotTerminate {
+
+ public static class ExitTrappedException extends SecurityException {
+
+  private static final long serialVersionUID = 1;
+ }
+
+ public static void forbidExit() {
+  final SecurityManager securityManager = new SecurityManager() {
+   @Override
+   public void checkPermission(Permission permission) {
+    if (permission.getName().contains("exitVM")) {
+     throw new ExitTrappedException();
+    }
+   }
+  };
+  System.setSecurityManager(securityManager);
+ }
+}
